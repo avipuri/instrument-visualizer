430cf84a7624e6add1a577391e0346a61e7dc42f
Juan408AR <aguirre.juanmp15@gmail.com>
2022-04-28 09:20:08 -0700

    Changing styling for vizualizer

--- client/src/visualizers/juanaguirre0711.tsx
+++ client/src/visualizers/juanaguirre0711.tsx
@@ -11,7 +11,7 @@ export const Testing = new Visualizer(
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
 
-    p5.background(0, 0, 0);
+    p5.background('rgba(100%, 0%, 100%, 0.5)');
     p5.translate(width / 2.5, height / 2);
     p5.noiseDetail(4, 4);
 
@@ -20,7 +20,7 @@ export const Testing = new Visualizer(
 
     var space = 1
 
-    for (let i = 0; i < 180; i += space) {
+    for (let i = 0; i < 200; i += space) {
       var amplitude = values[i] as number;
       var my_amp = Math.abs(amplitude)
 
@@ -29,20 +29,22 @@ export const Testing = new Visualizer(
       var n = p5.noise(xoff + start, yoff + start) * (my_amp * 10)
 
       var h = p5.map(n, 0, 1, 0, 10)
-      var w = 5
+      var w = 1
 
       var r = p5.map(Math.sin(i), 39, 1, 100, my_amp * 200)
-      var g = p5.map(i / 8, 52, 234, 0, my_amp * 200)
+      var g = p5.map(8, 52, 234, 0, my_amp * 200)
       var b = p5.map(n, 52, 235, 140, my_amp * 200)
       
       p5.stroke(24, 56, 100);
       p5.fill(r, g, b);
 
       p5.rotate(space);
-      p5.rect(20, 50, h, w);
+      p5.rect(20, 50, h, w, 80);
 
     }
     start += 10;
+
+    
 
     p5.endShape();
 
